0c8066153515252c2e0031da97b4b4b5265a742d
Mike Paiva <mikepaiva@resnet300-passy-ip-10-36-33.153.resnet.yorku.ca>
2021-03-04 19:38:44 -0500

    flask setup + audio file input

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        print("Form data received!")
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
